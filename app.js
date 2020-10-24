41aa55b2d84a486eb526ce4c8c87f0d26f788c30
brendanwong-web <bredanwong24@gmail.com>
2020-10-24 22:59:33 +0800

    big changes again

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ $(document).ready(function(){
 
       var hash = this.hash;
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1000, function(){
-        window.location.hash = hash;
-      });
+        scrollTop: $(hash).offset().top-65
+      }, 1000);
     }
   });
 
